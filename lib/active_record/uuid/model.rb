70a7c6c56523219b1908b3c0ce8c2077fd6bfdee
Nick Maloney <ngmaloney@gmail.com>
2013-02-11 13:40:09 -0500

    Allow uuid to be specified during initialization but not changed once object is persisted.

--- lib/active_record/uuid/model.rb
+++ lib/active_record/uuid/model.rb
@@ -5,18 +5,24 @@ module ActiveRecord
     module Model
       def self.included(model)
         model.instance_eval do
-          # This should never be user-assigned
-          attr_protected :uuid
+          attr_accessible :uuid
           # Validate on update since uuid won't exist yet on creation
           validates :uuid, :presence => true, :uniqueness => true, :on => :update
+          validate :uuid_not_changed
           before_create :assign_uuid
         end
       end
-    
+
     private
-    
+
       def assign_uuid
-        write_attribute(:uuid, SecureRandom.uuid)
+        write_attribute(:uuid, SecureRandom.uuid) unless uuid
+      end
+
+      def uuid_not_changed
+        if uuid_changed? && self.persisted?
+          errors.add(:uuid, "Change of uuid not allowed.")
+        end
       end
     end
   end
